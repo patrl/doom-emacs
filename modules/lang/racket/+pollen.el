4669b9b9e609d8dd981d0f3ac5e597159bdea95e
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-04-16 11:15:40 -0400

    Made start on a dedicated pollen mode

---
+++ modules/lang/racket/+pollen.el
@@ -0,0 +1,10 @@
+;;; lang/racket/+pollen.el --- description -*- lexical-binding: t; -*-
+
+(def-package! pollen-mode
+  :mode (("\\.pp$" . pollen-mode)))
+
+(def-package! company-pollen
+  :when (featurep! :completion company)
+  :after pollen-mode
+  :config
+  (set-company-backend! 'pollen-mode 'company-pollen))
